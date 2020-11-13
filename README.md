c152abf62a775abff3b46ff7e4db8bd0bd2701af
Adrian Kolano <kolanoadrian@hotmail.com>
2020-11-13 18:13:12 +0100

    last stage of the project completed

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Render-DOM
+# Document Object Model
 
-## https://kolanoadrian.github.io/Render-DOM/.
+## <a href="https://kolanoadrian.github.io/Render-DOM/">Go to the website</a>
+
+
+
+### Preview
+![Preview GIF](video1.gif)
+
+### Technologies Used
+
+- HTML
+- CSS
+- JS
+- scss
+- BEM 
+
+
+### Author
+Adrian Kolano 
+
+
+
+### Contact
+contact me at: [myopenpost@hotmail.com](myopenpost@hotmail.com)
--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const init = () => {
 
 init();
 
+// .....
 
 const addForm = document.querySelector(".form--add")
 addForm.addEventListener("submit", (event) => addElement(
@@ -36,7 +37,7 @@ const addElement = (event, node, txt, attr, value) => {
     }
     document.querySelector(".content").appendChild(elementNode);
 };
-
+// ....
 const serchForm = document.querySelector(".form--search")
 serchForm.addEventListener("submit", (event) => serchElements(event, serchForm.elements["serching-element"].value));
 
@@ -44,12 +45,13 @@ serchForm.addEventListener("submit", (event) => serchElements(event, serchForm.e
 const serchElements = (event, nameElement) => {
     event.preventDefault();
     const infoElement = document.querySelector(".result")
+    infoElement.textContent = '';
     const elements = document.querySelectorAll(nameElement)
     console.log(elements);
     if (elements.length) {
-        infoElement.innerHTML = `<p class="result__info">W tym dokumencie znalazłem ${elements.length}elenentów ${nameElement}</p>`;
-    schowInfo();
-    } else  { infoElement.innerHTML = `<p class="result__info">W tym dokumencie znalazłem NEIE ZNALAZŁEM elenentów ${nameElement}</p>`;
+        infoElement.innerHTML = `<h1 class="result__info">W tym dokumencie znalazłem ${elements.length} elenentów <strong>${nameElement}</strong></h1>`;
+    schowInfo(elements, infoElement);
+    } else  { infoElement.innerHTML = `<h1 class="result__info">W tym dokumencie Nie Znalazłem elenentów <strong>${nameElement}</strong></h1>`;
     return;
 }
 };
@@ -57,7 +59,24 @@ const serchElements = (event, nameElement) => {
 
 
 
-const schowInfo = () => {console.log("funkcja show info działa") }
+const schowInfo = (elements, infoElement) => {
+console.log("funkcja show info działa"),
+console.log(elements) 
+elements.forEach(element => {
+    const iteam = document.createElement("div");
+    iteam.className = "element-info";
+    iteam.innerHTML = `
+    <div>${element.nodeName}</div>
+    <div> klasy: ${element.className}</div>
+    <div> Wysokość Elementu (offsetHeight)${element.offsetHeight}</div>
+    <div> Szerokość Elementu (offsetWidth)${element.offsetWidth}</div>
+    <div> Odległość Elementu od lewej krawędzi (offsetLeft) ${element.offsetLeft}</div>
+    <div> Odległość Elementu od góry (offsetTop) ${element.offsetTop}</div>
+    <div> Liczba elemętów dzieci (childElementCount)${element.childElementCount}</div>
+    `;
+    infoElement.appendChild(iteam);
+});
+};
 
 
 
